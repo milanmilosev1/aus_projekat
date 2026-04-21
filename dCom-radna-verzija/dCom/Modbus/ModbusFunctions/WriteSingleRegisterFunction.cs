fef73184714616b7812ad7b267fc5e9177d5632d
milanmilosev1 <milosevmil@gmail.com>
2026-04-21 16:39:23 +0200

    WriteSingleRegister implemented

--- dCom-radna-verzija/dCom/Modbus/ModbusFunctions/WriteSingleRegisterFunction.cs
+++ dCom-radna-verzija/dCom/Modbus/ModbusFunctions/WriteSingleRegisterFunction.cs
@@ -24,15 +24,50 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters writeParams = (ModbusWriteCommandParameters)CommandParameters;
+
+            byte[] request = new byte[12];
+            int offset = 0;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.TransactionId)), 0, request, offset, 2);
+            offset += 2;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.ProtocolId)), 0, request, offset, 2);
+            offset += 2;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.Length)), 0, request, offset, 2);
+            offset += 2;
+
+            request[offset++] = writeParams.UnitId;
+            request[offset++] = writeParams.FunctionCode;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.OutputAddress)), 0, request, offset, 2);
+            offset += 2;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.Value)), 0, request, offset, 2);
+            offset += 2;
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            var result = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            if ((response[7] & 0x80) != 0)
+            {
+                HandeException(response[8]);
+            }
+
+            ushort address = (ushort)((response[8] << 8) | response[9]);
+            ushort value = (ushort)((response[10] << 8) | response[11]);
+
+            result.Add(
+                new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, address),
+                value);
+
+            return result;
         }
     }
 }
