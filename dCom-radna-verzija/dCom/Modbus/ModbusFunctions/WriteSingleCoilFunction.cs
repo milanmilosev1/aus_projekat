744367fc893f0f2c130587677c1b1dd763af7642
milanmilosev1 <milosevmil@gmail.com>
2026-04-21 16:33:50 +0200

    WriteSingleCoil function implemented

--- dCom-radna-verzija/dCom/Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
+++ dCom-radna-verzija/dCom/Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 namespace Modbus.ModbusFunctions
 {
     /// <summary>
-    /// Class containing logic for parsing and packing modbus write coil functions/requests.
+    /// Class containing logic for parsing and packing modbus write single coil functions/requests.
     /// </summary>
     public class WriteSingleCoilFunction : ModbusFunction
     {
@@ -24,15 +24,53 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters writeParams = (ModbusWriteCommandParameters)CommandParameters;
+
+            byte[] request = new byte[12];
+            int offset = 0;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.TransactionId)), 0, request, offset, 2);
+            offset += 2;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.ProtocolId)), 0, request, offset, 2);
+            offset += 2;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.Length)), 0, request, offset, 2);
+            offset += 2;
+
+            request[offset++] = writeParams.UnitId;
+            request[offset++] = writeParams.FunctionCode;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.OutputAddress)), 0, request, offset, 2);
+            offset += 2;
+
+            ushort coilValue = writeParams.Value == 0 ? (ushort)0x0000 : (ushort)0xFF00;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)coilValue)), 0, request, offset, 2);
+            offset += 2;
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            var result = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            if ((response[7] & 0x80) != 0)
+            {
+                HandeException(response[8]);
+            }
+
+            ushort address = (ushort)((response[8] << 8) | response[9]);
+            ushort rawValue = (ushort)((response[10] << 8) | response[11]);
+
+            ushort normalizedValue = rawValue == 0xFF00 ? (ushort)1 : (ushort)0;
+
+            result.Add(
+                new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, address),
+                normalizedValue);
+
+            return result;
         }
     }
 }
